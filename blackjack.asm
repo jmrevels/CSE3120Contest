5fca8bdd28e77c8d12137add0651b1c98fcd5fed
Joshua Revels <139186811+jmrevels@users.noreply.github.com>
2026-04-01 22:37:42 -0400

    Added Dealer score in .data, Added descriptions for each code block

--- blackjack.asm
+++ blackjack.asm
@@ -3,13 +3,29 @@ INCLUDE Irvine32.inc
 .data
 	; Player score total: Word 0
 	Score WORD 0
+	; Dealer score total: Word 0
+	DealerScore WORD 0
 	; Deck: 52-long array, byte each [May be removed later for simplicity]
 	Deck BYTE 52 DUP(?)
 	; # of aces (11's) dealt: byte [needed for reducing 11's to 1's; may not be added for simplicity]
 	Aces BYTE 0
 
 .code
+	; Proc Draw -- Draws card from Deck (generates value up to 51 & returns corresponding value in Deck)
+	;	- Should also set corresponding value in deck to -1 to indicate it is used
+	;	- Should jump back to start if -1 is drawn 
 Main PROC
+	; Display opening message + controls
+	; Start accepting input for hit (H) or stand (empty)
+	; Jump to respective code block depending on input
+	;	Hit: Draws card -> adds card to score -> inc Aces if needed -> reduce Ace to 1 if needed
+	;	Stand: Starts drawing for dealer, compares player/dealer score, jumps to ending (win/lose/draw)
+	
+	; Ending Cases
+	;	Blackjack: Occurs when player score is 21 at dealing, instant win (or tie if dealer also has blackjack)
+	;	Win: Occurs when player score is higher than dealer
+	;	Lose: Occurs when player score is lower than dealer
+	;	Push: Occurs when scores are tied
 
     ; INVOKE ExitProcess,0
 	exit
