3290a34ef714e82fde1888b96b6768b1fb983826
Joshua Revels <139186811+jmrevels@users.noreply.github.com>
2026-04-01 22:14:25 -0400

    Added .data Variables

--- blackjack.asm
+++ blackjack.asm
@@ -1,6 +1,12 @@
 INCLUDE Irvine32.inc
 
 .data
+	; Player score total: Word 0
+	Score WORD 0
+	; Deck: 52-long array, byte each [May be removed later for simplicity]
+	Deck BYTE 52 DUP(?)
+	; # of aces (11's) dealt: byte [needed for reducing 11's to 1's; may not be added for simplicity]
+	Aces BYTE 0
 
 .code
 Main PROC
